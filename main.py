acd3ea3fb570b9a1026789903436a854747d0f4e
Your Name <you@example.com>
2022-02-16 23:51:30 +0200

    new1

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 700
+
+screen= pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('MY GAME')
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+pygame.quit()
 
