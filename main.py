a0944729f20b75df294a908ddcd6d0608d2b84b5
hila214 <97167109+hila214@users.noreply.github.com>
2022-02-23 14:32:22 +0200

    update

--- main.py
+++ main.py
@@ -1,36 +1,16 @@
 import pygame
+
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 700
 
 screen= pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Shooter')
-
-
-class Soldier(pygame.sprite.Sprite):
-    def __init__(self, x, y, scale):
-        pygame.sprite.Sprite.__init__(self)
-        img = pygame.image.load('img player Idle 0.png')
-        self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
-        self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
-
-    def draw(self):
-            screen.blit(self.image, self.rect)
-
-
-
-player = Soldier(200, 200, 3)
- 
-
+pygame.display.set_caption('Tile based')
 
 
 run = True
 while run:
-
-    player.draw()
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
